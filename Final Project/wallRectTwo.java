289c9e01c7e6eacf17820fbb0b309118d1607f79
Divya-Bharti <31812888+Divya-Bharti@users.noreply.github.com>
2018-04-11 11:08:05 -0700

    Adding 2nd wall for game

---
+++ Final Project/wallRectTwo.java
@@ -0,0 +1,13 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/*
+ * The object that is produced for the maze for level two.
+ */
+public class wallRectTwo  extends wallRect
+{
+    // Method called whenever act or run is pressed.
+    public void act() 
+    {
+        // No action.
+    }    
+}
