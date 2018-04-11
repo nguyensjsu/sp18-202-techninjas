37f0573dff79a93746f327e19f92d2583c8c2bce
UttaraKulkarni <uttaravishwas.kulkarni@sjsu.edu>
2018-04-10 21:50:28 -0700

    End_L1_Prepare_L2

--- Levels.java
+++ Levels.java
@@ -4,11 +4,11 @@ public class Levels extends World
 {
 	public int level = 1;
 	public Levels()
-    {    
+    {
         super(400,400, 1);
         prepareLevelOne();
     }
-	
+
 	public void prepareLevelOne()
 	{
 		wallRectOne block ;
@@ -18,4 +18,20 @@ public class Levels extends World
             addObject(block, 575, 27 * i) ;
         }
 	}
+
+	// Component: End Level 1 and Prepare Level 2.
+	public void endLevelOne()
+	{
+			removeObjects(getObjects(wallRect.class));
+			removeObjects(getObjects(Enemy.class));
+			removeObjects(getObjects(PlayerOne.class));
+			removeObjects(getObjects(Home.class));
+			prepareLevelTwo();
+	}
+
+	public void prepareLevelTwo()
+	{
+			 wallRectTwo block ;
+	}
+
 }
