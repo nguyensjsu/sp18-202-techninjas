a3f4aba436eec4a5ce4fe4041ee0f9b67acc68cb
UttaraKulkarni <uttaravishwas.kulkarni@sjsu.edu>
2018-04-14 19:42:07 -0700

    Level2_v.12

--- Final Project/Levels.java
+++ Final Project/Levels.java
@@ -23,13 +23,13 @@ public class Levels extends World
             block = new wallRectOne() ;
             addObject(block, 27 * i, 50) ;
         }
-		
+
 		for (int i = 2 ; i < 22 ; i++)
         {
             block = new wallRectOne() ;
             addObject(block, 27 * i, 550) ;
-        } 
-		
+        }
+
 		PlayerOne player = new PlayerOne() ;
         addObject(player,75,315) ;
 	}
@@ -47,12 +47,39 @@ public class Levels extends World
 	public void prepareLevelTwo()
 	{
 		wallRectTwo block ;
-		
+		//vertical
 		for (int i = 6 ; i < 21 ; i++)
         {
             block = new wallRectTwo() ;
             addObject(block, 575, 27 * i) ;
         }
+		//vertical
+		for (int i = 2 ; i < 17 ; i++)
+			  {
+			      block = new wallRectTwo() ;
+			      addObject(block, 53, 27 * i) ;
+			  }
+		//horizontal
+		for (int i =2 ; i < 22 ; i++)
+				{
+				    block = new wallRectTwo() ;
+				    addObject(block, 27 * i, 50) ;
+				}
+	//horizontal
+		for (int i = 2 ; i < 22 ; i++)
+				{
+				     block = new wallRectTwo() ;
+				     addObject(block, 27 * i, 550) ;
+				}
+
 	}
+	public void endLevelTwo()
+    {
+        removeObjects(getObjects(wallRect.class));
+        removeObjects(getObjects(Enemy.class));
+        removeObjects(getObjects(PlayerTwo.class));
+        removeObjects(getObjects(Home.class));
+        prepareLevelThree();
+    }
 
 }
